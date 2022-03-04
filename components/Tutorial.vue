0e61efe31d659e6921f16c54ca2a8c4647ad73c6
serkanurganci <serkan.urganci10@gmail.com>
2022-03-04 23:00:40 +0300

    initial commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="h-screen text-7xl flex items-center justify-center">
+    Todo App
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
+
