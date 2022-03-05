00cbb64011e20408d925403042a49dfa59332043
serkanurganci <serkan.urganci10@gmail.com>
2022-03-05 10:10:18 +0300

    complete AddTodo component styles

--- components/AddTodo.vue
+++ components/AddTodo.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="add-todo">Add Todo</div>
+  <div
+    class="add-todo"
+  >
+        <span
+          class="add-todo__check-icon"
+
+        ></span>
+    <form class="add-todo__form">
+      <input
+        type="text"
+        placeholder="Create a new todo..."
+        class="input"
+        v-model="todoInput"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "AddTodo"
+  name: "AddTodo",
+  data(){
+    return{
+      todoInput:null
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .add-todo{
-  @apply text-7xl text-white;
-
+  @apply rounded-md h-14 transition-colors relative bg-white bg-gray-800 p-3 flex items-center ;
+  &__check-icon{
+    @apply w-5 h-5 bg-transparent border border-gray-600 rounded-full inline-block cursor-pointer;
+  }
+  &__form{
+    @apply w-full h-full ml-5;
+    .input{
+      @apply bg-transparent w-full h-full inline-block text-gray-300 focus-visible:outline-none   ;
+    }
+  }
 }
 </style>
